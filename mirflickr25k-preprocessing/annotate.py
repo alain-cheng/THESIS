7d38376b0efcccc9df6669dce89004bef34f73fa
Alain Cheng <104754302+alain-cheng@users.noreply.github.com>
2024-05-31 19:10:40 +0800

    fix: add missing file 'annotate.py'

---
+++ mirflickr25k-preprocessing/annotate.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+
+save_dir = None
+
+def annotate(color=(0, 0, 0), size=(400, 400), save_name=None, save_dir=None):
+    """
+    Input Params:
+        color - a tuple containing three integers corresponding to RGB values
+        size - a tuple containing the output width and height image
+        save_name - a string; image name
+        save_dir - a string; the output directory of the annotation
+    """
+    # Note: were actually just creating solid 400x400 blocks of color as
+    #       the encoding will encompass the entire 400x400 image.
+
+    if save_dir is not None:
+        if save_name is not None:
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+        
+            label = Image.new('RGB', (size[0],size[1]), (color[0], color[1], color[2]))
+            label.save(save_dir + '/' + save_name + '_L' + '.jpg')
+        else:
+            print("save_name is not defined")
+    else:
+        print("save_dir is not defined")
+    
